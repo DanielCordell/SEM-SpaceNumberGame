2ac9b94da179d620e70a38afcfc5d8f7574d8fb6
Daniel <danielbc@hotmail.co.uk>
2020-02-19 00:40:37 +0000

    Asteroids can rotate in multiple directions!

--- Unity Project/Space Numbers Game/Assets/Scripts/Misc/Rotate.cs
+++ Unity Project/Space Numbers Game/Assets/Scripts/Misc/Rotate.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+
+
     public float rotationSpeed;
+
     [RangeAttribute(0.5f, 1.5f)]
     public float randomRotationScale;
-    // Update is called once per frame
+
+    private int direction;
 
     void Start()
     {
         randomRotationScale = Random.Range(0.5f, 1.5f);
+        direction = Random.value < 0.5 ? -1 : 1;
     }
     void Update()
     {
-        transform.Rotate(0, 0, rotationSpeed * randomRotationScale);
+        transform.Rotate(0, 0, rotationSpeed * randomRotationScale * direction);
     }
 }
