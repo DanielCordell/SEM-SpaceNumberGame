549db1c368ebee8c309e503386a8412591e5ed09
Yao Fang <flavioy@live.com>
2020-02-25 19:15:54 +0000

    Implement shield state control

--- Unity-Project/Space-Numbers-Game/Assets/Resources/Scripts/Shield/Shield.cs
+++ Unity-Project/Space-Numbers-Game/Assets/Resources/Scripts/Shield/Shield.cs
@@ -5,37 +5,78 @@ using UnityEngine.UI;
 
 public class Shield : MonoBehaviour
 {
-    public Image healthBar;
+
+    // This variable receives values of how many time does the user wrongly answer.
     public int countWrong;
+
+    public GameObject healthBarFull;
+    public GameObject healthBar_2_3;
+    public GameObject healthBar_1_3;
+    public GameObject shield_full;
+    public GameObject shield_2_3;
+    public GameObject shield_1_3;
+    public GameObject shield_destoryed;
     // Start is called before the first frame update
     void Start()
     {
-        healthBar = GetComponent<Image>();
-       
+        healthBarFull = transform.Find("HealthBar_full").gameObject;
+        healthBar_2_3 = transform.Find("HealthBar_2_3").gameObject;
+        healthBar_1_3 = transform.Find("HealthBar_1_3").gameObject;
+        shield_full = GameObject.FindGameObjectWithTag("shield_full").gameObject;
+        shield_2_3 = GameObject.FindGameObjectWithTag("shield_2_3").gameObject;
+        shield_1_3 = GameObject.FindGameObjectWithTag("shield_1_3").gameObject;
+        shield_destoryed = GameObject.FindGameObjectWithTag("shield_destoryed").gameObject;
     }
+
     public void UpdateHealthBar(int countWrong)
     {
         switch (countWrong)
         {
             case 0:
+                shield_full.SetActive(true);
+                shield_2_3.SetActive(false);
+                shield_1_3.SetActive(false);
+                shield_destoryed.SetActive(false);
+                healthBarFull.SetActive(true);
+                healthBar_2_3.SetActive(false);
+                healthBar_1_3.SetActive(false);
                 break;
             case 1:
-                healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(140 * 2 / 3, 20);
+                shield_full.SetActive(false);
+                shield_2_3.SetActive(true);
+                shield_1_3.SetActive(false);
+                shield_destoryed.SetActive(false);
+                healthBarFull.SetActive(false);
+                healthBar_2_3.SetActive(true);
+                healthBar_1_3.SetActive(false);
                 break;
             case 2:
-                healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(140 * 1 / 3, 20);
+                shield_full.SetActive(false);
+                shield_2_3.SetActive(false);
+                shield_1_3.SetActive(true);
+                shield_destoryed.SetActive(false);
+                healthBarFull.SetActive(false);
+                healthBar_2_3.SetActive(false);
+                healthBar_1_3.SetActive(true);
                 break;
             default:
-                healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(0 , 20);
+                shield_full.SetActive(false);
+                shield_2_3.SetActive(false);
+                shield_1_3.SetActive(false);
+                shield_destoryed.SetActive(true);
+                healthBarFull.SetActive(false);
+                healthBar_2_3.SetActive(false);
+                healthBar_1_3.SetActive(false);
                 break;
 
 
         } 
     }
+
     // Update is called once per frame
     void Update()
     {
-        countWrong = 2;
+        countWrong = 3;
         UpdateHealthBar(countWrong);
     }
 }
