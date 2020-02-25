33b4ff0ef0d013b4c734373174d1e0a68107ef1a
zenevip <zyzxin@qq.com>
2020-02-25 18:13:07 +0000

    Creat shield.cs

---
+++ Unity-Project/Space-Numbers-Game/Assets/Resources/Scripts/Shield/Shield.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shield : MonoBehaviour
+{
+    public Image healthBar;
+    public int countWrong;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar = GetComponent<Image>();
+       
+    }
+    public void UpdateHealthBar(int countWrong)
+    {
+        switch (countWrong)
+        {
+            case 0:
+                break;
+            case 1:
+                healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(140 * 2 / 3, 20);
+                break;
+            case 2:
+                healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(140 * 1 / 3, 20);
+                break;
+            default:
+                healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(0 , 20);
+                break;
+
+
+        } 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        countWrong = 2;
+        UpdateHealthBar(countWrong);
+    }
+}
