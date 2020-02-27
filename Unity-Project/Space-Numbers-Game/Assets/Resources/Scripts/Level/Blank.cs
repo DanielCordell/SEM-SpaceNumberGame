47ce7cb1a37821c76ce79a4973744859c4e44070
Yao Fang <flavioy@live.com>
2020-02-27 17:24:56 +0000

    rebased from dev, add animation of shield, healthbar

---
+++ Unity-Project/Space-Numbers-Game/Assets/Resources/Scripts/Level/Blank.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blank : MonoBehaviour
+{
+    int? value;
+    Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.transform.Find("Text").gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        text.text = value == null ? "" : value.ToString();
+    }
+
+    public int? GetValue()
+    {
+        return value;
+    }
+
+    public void SetValue(int input)
+    {
+        value = input;
+    }
+
+    public bool IsEmpty()
+    {
+        return value == null;
+    }
+
+    public void ClearValue()
+    {
+        value = null;
+    }
+}
