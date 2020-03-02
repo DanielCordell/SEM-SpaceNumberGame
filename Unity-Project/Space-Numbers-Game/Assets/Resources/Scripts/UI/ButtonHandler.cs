b48adeab48d3b61f43a26e6a005344ad37e4614a
Daniel <danielbc@hotmail.co.uk>
2020-03-02 15:44:00 +0000

    Implemented main menu + performed asset fixes

---
+++ Unity-Project/Space-Numbers-Game/Assets/Resources/Scripts/UI/ButtonHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    ParticleSystem[] particleSystems;
+    public void Start()
+    {
+        particleSystems = gameObject.GetComponentsInChildren<ParticleSystem>();
+    }
+
+    public void StartParticles()
+    {
+        foreach (ParticleSystem ps in particleSystems)
+        {
+            ps.enableEmission = true;
+        }
+    }
+
+    public void StopParticles()
+    {
+        foreach (ParticleSystem ps in particleSystems)
+        {
+            ps.enableEmission = false;
+        }
+    }
+
+    public void MoveCamera(Transform t)
+    {
+        Camera.main.transform.position = t.position;
+    }
+}
