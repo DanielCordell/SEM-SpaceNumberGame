16d8a441beb105e0dc987793ac113c55325ceea9
Daniel <danielbc@hotmail.co.uk>
2020-03-04 14:43:17 +0000

    Removed old music, implemented new music switching.
    On start, chooses a random track, plays that. Once finished, switches to the other one.

--- Unity-Project/Space-Numbers-Game/Assets/Resources/Scripts/Tools/GameMusicShuffler.cs
+++ Unity-Project/Space-Numbers-Game/Assets/Resources/Scripts/Tools/GameMusicShuffler.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class GameMusicShuffler : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    AudioSource audioSource;
+
+    AudioClip[] audioClips = new AudioClip[2];
+    int currentAudioIndex;
+
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        audioClips[0] = Resources.Load<AudioClip>("Audio/Music/Background_Game1 - (Dream about space)");
+        audioClips[1] = Resources.Load<AudioClip>("Audio/Music/Background_Game2 - (I didn‘t do it)");
+        currentAudioIndex = Random.Range(0, 2);
+        Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!audioSource.isPlaying)
+        {
+            currentAudioIndex = currentAudioIndex == 1 ? 0 : 1;
+            Play();
+        }
+    }
+
+    void Play()
+    {
+        audioSource.clip = audioClips[currentAudioIndex];
+        audioSource.Play();
     }
 }
