9de9f84270213cc700f927d60e6863a36f03df8a
Alex Johnson <alextheviking@hotmail.co.uk>
2020-03-04 15:44:13 +0000

    Level info populated when level button is pressed

---
+++ Unity-Project/Space-Numbers-Game/Assets/Resources/Scripts/UI/LevelInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelInfo : MonoBehaviour
+{
+    //PlayButton playButton
+    Text level;
+    Text difficulty;
+    Text numbers;
+    Text operators;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject levelInfo = gameObject.transform.Find("LevelInfo").gameObject;
+
+        level = levelInfo.transform.Find("LevelNumber").gameObject.GetComponent<Text>();
+        level.text = "";
+        difficulty = levelInfo.transform.Find("Difficulty").gameObject.GetComponent<Text>();
+        difficulty.text = "";
+        numbers = levelInfo.transform.Find("Numbers").gameObject.GetComponent<Text>();
+        numbers.text = "";
+        operators = levelInfo.transform.Find("Operators").gameObject.GetComponent<Text>();
+        operators.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateInfo(int levelNumber, Difficulty levelDifficulty, NumberRange numberRange, List<Operator> levelOperators)
+    {
+        Debug.Log("Updating Info Pane");
+        level.text = "Level " + levelNumber.ToString();
+        difficulty.text = levelDifficulty.ToString();
+        numbers.text = "Numbers " + numberRange.RangeFloor.ToString() + " to " + numberRange.RangeCeiling.ToString();
+        operators.text = String.Join("   ", levelOperators.Select(o => {
+            return o.ToOpString();
+        }));
+
+        //Update play button
+    }
+}
---
+++ Unity-Project/Space-Numbers-Game/Assets/Resources/Scripts/UI/LevelSelectButtonHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectButtonHandler : MonoBehaviour
+{
+    int levelNumber;
+    Difficulty levelDifficulty;
+    NumberRange numberRange;
+    List<Operator> operators;
+
+    public LevelInfo LevelInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelNumber = 1;
+        levelDifficulty = Difficulty.Easy;
+        numberRange = new NumberRange{ RangeFloor = 1, RangeCeiling = 10};
+        operators = new List<Operator>{ Operator.Add };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetLevelInfo() {
+        //Update level info pane with info
+        LevelInfo.UpdateInfo(levelNumber, levelDifficulty, numberRange, operators);
+    }
+}
+
+public class NumberRange 
+{
+    public int RangeFloor { get; set; }
+    public int RangeCeiling { get; set; }
+}
+
+public enum Difficulty
+{
+    Easy, 
+    Medium,
+    Hard,
+    Extreme
+}
+
+public static class DifficultyExtensions
+{
+    public static string ToString(this Difficulty difficulty)
+    {
+        switch (difficulty)
+        {
+            case Difficulty.Easy: return "Easy";
+            case Difficulty.Medium: return "Medium";
+            case Difficulty.Hard: return "Hard";
+            case Difficulty.Extreme: return "Extreme";
+            default: throw new ArgumentException("Invalid difficulty: " + difficulty.ToString());
+        }
+    }
+}
